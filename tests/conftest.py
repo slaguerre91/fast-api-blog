79790e1d0cdedb61402bca28ff79863cb6d11fdc
unknown <samyrmlaguerre@gmail.com>
2024-01-28 17:57:54 -0500

    CI/CD

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ import pytest
 from app.oauth2 import create_token
 from app import models
 
-SQLALCHEMY_DATABASE_URL = "f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'"
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
