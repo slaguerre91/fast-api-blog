774c6a16dbebb1694147e4983513dd5efe7d39d7
Samyr Laguere <samyrmlaguerre@gmail.com>
2024-03-12 13:05:25 -0400

    Removed unused Docker components

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM python:3.9.7
-WORKDIR /usr/src/app
-COPY requirements.txt ./
-RUN pip install --no-cache-dir -r requirements.txt
-COPY . .
-RUN alembic upgrade head
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- tests/test_calculations.py
+++
@@ -1,9 +0,0 @@
-# from app.calculations import add
-# import pytest
-
-# @pytest.mark.parametrize('num1, num2, expected', [
-#     (4, 5, 9), (7, 2, 9), (8, 34, 42)
-# ])
-# def test_add(num1, num2, expected):
-#     sum = add(num1, num2)
-#     assert sum == expected
